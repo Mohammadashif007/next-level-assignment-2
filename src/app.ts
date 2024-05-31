f70b1ffa5b238829a210340cc310528c084f991e
Mohammad Ashif <Mohammad Ashif>
2024-05-31 11:40:11 +0600

    edit done

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,18 @@ const app: Application = express();
 
 app.use(express.json());
 
-app.use("/api/products", ProductRoutes)
-app.use("/api/orders", OrderRouter)
-app.get("/", (req: Request, res: Response) => {
-    res.send("Product server");
-})
-
+app.use('/api/products', ProductRoutes);
+app.use('/api/orders', OrderRouter);
 
 app.use((req, res) => {
-    res.status(404).json({
-        success: false,
-        message: "Route not found",
-    })
-})
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
+});
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Product server');
+});
 
 export default app;
